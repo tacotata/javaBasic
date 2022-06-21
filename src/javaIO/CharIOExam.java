a998a5ef119882a4ca5988858c35e0eec2733a7c
tacotata <71251163+tacotata@users.noreply.github.com>
2022-06-21 20:28:00 +0900

    import 제거

--- src/javaIO/CharIOExam.java
+++ src/javaIO/CharIOExam.java
@@ -1,8 +1,7 @@
 package javaIO;
 
-import java.io.BufferedReader;
+
 import java.io.FileWriter;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 
@@ -10,18 +9,14 @@ import java.io.PrintWriter;
 public class CharIOExam {
 
 	 public static void main(String[]args){
-         BufferedReader br = null;
+         
          PrintWriter pw = null; 
-        String str = "안녕하세요. PrintWriter입니다.";
+         String str = "안녕하세요. PrintWriter입니다.";
         try{
-             //System.out.println("안녕하세요. PrintWriter입니다.");
-           // br = new BufferedReader(new InputStreamReader(System.));
             pw = new PrintWriter(new FileWriter("test.txt"));
-            String line = null;
-			//파일 끝이면 null 값 리턴함 
-			
-				pw.println(str);
-		
+            
+            String line = null;		
+			pw.println(str);
              
         }catch(Exception e ){
             e.printStackTrace();
@@ -29,7 +24,6 @@ public class CharIOExam {
             pw.close();
            
         }
-      
         
     }
 
