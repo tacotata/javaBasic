d3774fe6d7b311bebca4efec5f944756c7dc9876
tacotata <71251163+tacotata@users.noreply.github.com>
2022-06-24 20:42:06 +0900

    람다식

--- src/javaUtill_02/LambdaExam.java
+++ src/javaUtill_02/LambdaExam.java
@@ -3,15 +3,12 @@ package javaUtill_02;
 public class LambdaExam {
 
 	public static void main(String[] args) {
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
+		new Thread(()-> {
 				// TODO Auto-generated method stub
 				for(int i =0; i <10; i++) {
 					System.out.println("hello");
 				}
-			}
+			
 		}).start();
 
 	}
