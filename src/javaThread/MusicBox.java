ae47a60b69af6f202004075920dbce9bae2449ca
tacotata <71251163+tacotata@users.noreply.github.com>
2022-06-24 19:42:12 +0900

    쓰레드와 공유객체

---
+++ src/javaThread/MusicBox.java
@@ -0,0 +1,40 @@
+package javaThread;
+
+public class MusicBox {
+	
+	public void playMusicA() {
+		for(int i =0; i<10; i++) {
+			System.out.println("신나는 음악!!!");
+			try {
+				Thread.sleep((int)(Math.random()*1000));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void playMusicB() {
+		for(int i =0; i<10; i++) {
+			System.out.println("슬픈 음악!!!");
+			try {
+				Thread.sleep((int)(Math.random()*1000));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void playMusicC() {
+		for(int i =0; i<10; i++) {
+			System.out.println("카페 음악!!!");
+			try {
+				Thread.sleep((int)(Math.random()*1000));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
